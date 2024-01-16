f2016abd537ace2d26d3ade21abe775f38d876df
NanaPhiona <nankyaphio15@gmail.com>
2024-01-16 12:22:52 +0300

    Defining models, views and controllers for the project.

---
+++ InventoTracker/app/Models/Company.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    //boot
+    protected static function boot(){
+        parent::boot();
+
+        //creating (before)
+        static::updated(function  ($company){
+            $owner = User::find($company->owner_id);
+            if ($owner == null){
+                throw new \Exception('Owner not found');
+            }
+            $owner->company_id = $company->id;
+            $owner->save();
+        });
+
+    }
+}
